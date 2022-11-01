2075bcd248639e647aefcfa86d0b98eb943ca48c
wegar <artur.wegrzyn@axpo.pl>
2022-11-01 15:27:48 +0100

    Uploaded some notes on encoding and a new dataset from Kaggle

--- utils/package_data_loaders.py
+++ utils/package_data_loaders.py
@@ -2,6 +2,7 @@ import datetime as dt
 import pandas as pd
 import os
 from functools import reduce
+import numpy as np
 
 
 def df_load_stooq_time_series_data_from_file(str_file_path: str, str_stock_name: str) -> pd.DataFrame:
@@ -36,4 +37,18 @@ def df_load_stock_prices_time_series() -> pd.DataFrame:
         lambda df_l, df_r: pd.merge(df_l, df_r, on="quote_date", how="outer"),
         list(dict_data.values())
     )
+    return df
+
+
+def df_load_strokes_medical_data() -> pd.DataFrame:
+    """
+    Data set fetched from Kaggle: https://www.kaggle.com/datasets/fedesoriano/stroke-prediction-dataset
+    I do not own the data.
+    """
+    str_path_to_data_file = os.path.join(
+        os.path.dirname(os.path.dirname(__file__)), "data", "healthcare-dataset-stroke-data.csv")
+    df = pd.read_csv(str_path_to_data_file, na_values=[np.nan, "Unknown"])
+    df.dropna(subset=["bmi", "smoking_status"], inplace=True)
+    df.drop(columns=["id"], inplace=True)
+    df.columns = [el.lower() for el in list(df.columns)]
     return df
